40d7861695572cee5ac9897f167d57a37ec8a23f
turuthivic <turuthivictor9@gmail.com>
2017-09-18 18:49:29 +0300

    category relation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+	has_many :lost_items
 end
--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -2,4 +2,5 @@ class LostItem < ApplicationRecord
   # belongs_to :category
   # belongs_to :found_by
   belongs_to :user
+  belongs_to :category
 end
