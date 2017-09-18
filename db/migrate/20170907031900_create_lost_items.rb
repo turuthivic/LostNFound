3ee2abf4ebccca9585047d0dd19ce59eb12c5a42
turuthivic <turuthivictor9@gmail.com>
2017-09-18 19:18:42 +0300

    migration files

--- db/migrate/20170907031900_create_lost_items.rb
+++ db/migrate/20170907031900_create_lost_items.rb
@@ -3,8 +3,7 @@ class CreateLostItems < ActiveRecord::Migration[5.0]
     create_table :lost_items do |t|
       t.string :name
       t.references :category, foreign_key: true
-      t.references :found_by, foreign_key: true
-      t.references :owner, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
