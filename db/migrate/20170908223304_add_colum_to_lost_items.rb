9992bed043c338e3fef15ebf563f13b12c3be671
turuthivic <turuthivictor9@gmail.com>
2017-09-18 19:20:31 +0300

    migration files

--- db/migrate/20170908223304_add_colum_to_lost_items.rb
+++ db/migrate/20170908223304_add_colum_to_lost_items.rb
@@ -1,5 +1,5 @@
 class AddColumToLostItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :lost_items, :category_id, :integer
+    # add_column :lost_items, :category_id, :integer
   end
 end
