3efcd1c5849e5b0fa223fc4c90c30afc715e4402
turuthivic <turuthivictor9@gmail.com>
2017-09-19 04:21:32 +0300

    redirect after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_item
   before_action :set_category
+  layout :layout_by_activeness
 
+def after_sign_in_path_for(resource_or_scope)
+  lost_items_path
+end
   protected
 
   def set_item
@@ -13,4 +17,14 @@ class ApplicationController < ActionController::Base
   def set_category
   	@category = Category.where(@category_id)
   end
+
+private
+
+def layout_by_activeness
+  if devise_controller?
+     'application'
+  else
+     'application'
+  end
+end
 end
